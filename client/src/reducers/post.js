edb8ad3ca9ab260e2be3f8528055371e3e167de8
Andre Entrekin <andre.entrekin@gmail.com>
2020-05-05 16:28:46 -0500

    mvp

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,9 +1,12 @@
 import {
   GET_POSTS,
+  GET_POST,
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POSTS,
   ADD_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from "../actions/types";
 
 const initialState = {
@@ -21,6 +24,12 @@ export default function (state = initialState, action) {
       return {
         ...state,
         posts: payload,
+        loading: false,
+      };
+    case GET_POST:
+      return {
+        ...state,
+        post: payload,
         loading: false,
       };
     case ADD_POST:
@@ -49,7 +58,20 @@ export default function (state = initialState, action) {
         posts: state.posts.filter((post) => post._id !== payload),
         loading: false,
       };
-
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: { ...state.post, comments: payload },
+        loading: false,
+      };
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post.comments.filter((comment) => comment._id !== payload),
+        },
+        loading: false,
+      };
     default:
       return state;
   }
